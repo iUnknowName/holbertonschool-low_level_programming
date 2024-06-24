cd788cb3c620544f58af8e4f269a03857cf6e344
iUnknowName <IvanGomez1598@hotmail.com>
2024-06-23 21:07:05 -0500

    Function that prints the multiplication table n, starting with 0

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,9 +1,37 @@
 #include "main.h"
 
 /**
+ * print_number - Prints an integer with proper spacing.
+ * @num: The number to print.
+ */
+
+void print_number(int num)
+{
+	if (num < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(num + '0');
+	}
+	else if (num < 100)
+	{
+		_putchar(' ');
+		_putchar(num / 10 + '0');
+		_putchar(num % 10 + '0');
+	}
+	else
+	{
+		_putchar(num / 100 + '0');
+		_putchar((num / 10) % 10 + '0');
+		_putchar(num % 10 + '0');
+	}
+}
+
+/**
  * print_times_table - Prints the n times table, starting with 0.
  * @n: The number for the times table.
  */
+
 void print_times_table(int n)
 {
 	int i, j, product;
@@ -23,24 +51,7 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (product < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product + '0');
-			}
-			else if (product < 100)
-			{
-				_putchar(' ');
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
-			}
-			else
-			{
-				_putchar(product / 100 + '0');
-				_putchar((product / 10) % 10 + '0');
-				_putchar(product % 10 + '0');
-			}
+			print_number(product);
 		}
 		_putchar('\n');
 	}

74fe183d075959f5093df7d0851e57cbc35b6ed2
iUnknowName <IvanGomez1598@hotmail.com>
2024-06-23 20:29:06 -0500

    Function that prints the multiplication table n, starting with 0

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * print_times_table - Imprime la tabla de multiplicar de n, comenzando desde 0
- * @n: Número cuya tabla de multiplicar se imprimirá
- */
-
 void print_times_table(int n)
 {
+	int i, j;
+
 	if (n < 0 || n > 15)
 		return;
 
-	int i = 0;
-
-	for (; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 
 	{
 		printf("0");
-
-		int j = 0;
-
-		for (; j <= n; j++)
+		for (j = 0; j <= n; j++)
 
 		{
 			int product = i * j;

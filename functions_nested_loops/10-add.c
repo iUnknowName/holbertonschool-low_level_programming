76f343b530763f56e1d52e049b29cdcadb877193
iUnknowName <IvanGomez1598@hotmail.com>
2024-06-23 20:06:09 -0500

    Function that adds two integers and returns their result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Suma dos números enteros
+ * @a: Primer número a sumar
+ * @b: Segundo número a sumar
+ *
+ * Return: La suma de los dos números a y b
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}

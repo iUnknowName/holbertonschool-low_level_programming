1b65e1a148853b34c36a0612db7c1bafffaaa9e1
iUnknowName <IvanGomez1598@hotmail.com>
2024-06-23 19:33:40 -0500

    Function that prints the 9 multiplication table starting with 0

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - Imprime la tabla de multiplicar del 9, comenzando con 0
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar('0'); /* Imprime el primer número en cada fila */
+
+		for (b = 1; b < 10; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			c = a * b;
+
+			/* Asegura que el número esté formateado adecuadamente */
+			if (c < 10)
+			{
+				_putchar(' '); /* Espacio adicional para alinear correctamente */
+				_putchar(' '); /* Incluir espacio adicional si el número es < 10 */
+			}
+			else
+			{
+				_putchar((c / 10) + '0'); /* Imprime la decena */
+			}
+
+			_putchar((c % 10) + '0'); /* Imprime la unidad */
+		}
+
+		_putchar('\n'); /* Nueva línea al final de cada fila */
+	}
+}
